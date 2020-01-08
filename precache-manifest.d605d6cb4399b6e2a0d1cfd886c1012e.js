920c2434fc9837a89464e1a8f7f903a2a7805f9d
Amit <jumpbang.dev@gmail.com>
2020-01-08 10:07:38 +0545

    Updates

---
+++ precache-manifest.d605d6cb4399b6e2a0d1cfd886c1012e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "124050da8d972c2f3a0f80ecc0b35d29",
+    "url": "/react_fetch_api/index.html"
+  },
+  {
+    "revision": "72c9293a8f75c7bf2636",
+    "url": "/react_fetch_api/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "c1d0e38a16e8c6ca9b29",
+    "url": "/react_fetch_api/static/css/main.d9faf441.chunk.css"
+  },
+  {
+    "revision": "72c9293a8f75c7bf2636",
+    "url": "/react_fetch_api/static/js/2.17ff5fcb.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/react_fetch_api/static/js/2.17ff5fcb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c1d0e38a16e8c6ca9b29",
+    "url": "/react_fetch_api/static/js/main.346e31f1.chunk.js"
+  },
+  {
+    "revision": "402ee4468854508fb2f7",
+    "url": "/react_fetch_api/static/js/runtime-main.7f83447d.js"
+  }
+]);
